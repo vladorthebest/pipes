a9d443e6177862b25e4fba3686f4bd121c1da2c2
Vladyslav Nosov <vladorthebest@gmail.com>
2023-04-11 14:19:05 +0200

    Add game in main file

--- src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment2.java
@@ -1,7 +1,9 @@
 package sk.stuba.fei.uim.oop;
 
+import sk.stuba.fei.uim.oop.game.pipes.Pipes;
+
 public class Assignment2 {
     public static void main(String[] args) {
-        
+        new Pipes();
     }
 }
